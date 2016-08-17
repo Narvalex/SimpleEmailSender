d585c6899bddf2835d7ce594469023c64cf0da1f
Alexis Darién Narváez Gamarra <narvalex@hotmail.com>
2016-08-17 14:50:43 -0400

    testing with dumb data

---
+++ src/SimpleEmailSender.Core.Tests/Playground.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net.Mail;
+
+namespace SimpleEmailSender.Core.Tests
+{
+    [TestClass]
+    public class Playground
+    {
+        [TestMethod]
+        public void Can_send_mail()
+        {
+            var smtpClient = new SmtpClient("mail.fecoprod.comERCIAL.PARAG", 25);
+            smtpClient.UseDefaultCredentials = true;
+            smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+            smtpClient.EnableSsl = false;
+
+            var mail = new MailMessage();
+            mail.From = new MailAddress("chuchonavarro@fecoprod.com.py", "Test");
+            mail.To.Add(new MailAddress("chayala@fecoprod.com.py"));
+            mail.Subject = "hola";
+            mail.Body = "hola este es sin credenciales";
+
+            smtpClient.Send(mail);
+        }
+    }
+}
